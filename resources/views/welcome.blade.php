65048fd385945d0c8622f56ecb25c4ee23d830b7
elynnjj <syaz.elyna@gmail.com>
2025-12-09 23:37:49 +0800

    design pt4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>ExactAsset</title>
+
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="{{ asset('images/exact2.png') }}?v=exactasset">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
